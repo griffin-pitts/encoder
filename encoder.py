a2ce3e5b46651d643fef728daf134ef8c8135443
Griffin Pitts <griffpitts04@gmail.com>
2023-03-09 14:08:18 -0500

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -1,15 +1,47 @@
-# This is a sample Python script.
+# Create menu
+def menu():
+    print("""Menu  
+------------- 
+1. Encode  
+2. Decode  
+3. Quit""")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Encode password, add each number by 3
+def encode(password):
+    encoded = ""
+    for num in password:
+        if int(num) < 7:
+            new = str(int(num)+3)
+            encoded += new
+        elif int(num) == 7:
+            new = "0"
+            encoded += new
+        elif int(num) == 8:
+            new = "1"
+            encoded += new
+        elif int(num) == 9:
+            new = "2"
+            encoded += new
+    return(encoded)
 
-
+# Take input, provide encoding / decoding
 def main():
-    print("hi")
+    while True:
+        menu()
+        option = int(input("\nPlease enter an option: "))
+        #Encode
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded = encode(password)
+            print("Your password has been encoded and stored!\n")
+            continue
+        #Decode
+        if option == 2:
+            print(f"The encoded password is {encoded}, and the original password is {password}.\n")
+        #Exit
+        if option == 3:
+            return
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
